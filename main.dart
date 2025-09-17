8e691f1670712734f25f7249a1a200155f1138dd
Matias Scruz <144630285+matscruz@users.noreply.github.com>
2025-09-17 23:08:59 +0000

    feat: add exer add

--- main.dart
+++ main.dart
@@ -1,4 +1,11 @@
-void main(){
-    print("Olá mundo");
+import 'dart:io';
+void  main() {
+    int idade = 18;
+    if (idade > 18)
+    {
 
+     print('pode entrar na balada');
+
+    }
+    print('fim do programa');
 }
